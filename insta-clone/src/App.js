f7ae50fa0ccd0d98fd596d3ef2453ca3d89783e6
Carlossantana987 <carlossantana987@outlook.com>
2019-06-04 22:29:22 -0400

    changed a few things on App.js

--- insta-clone/src/App.js
+++ insta-clone/src/App.js
@@ -5,30 +5,24 @@ import SearchBar from "./components/SearchBar/SearchBar";
 import PostContainer from "./components/PostContainer/PostContainer";
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      data: []
-    };
-  }
+  state = {
+    data: []
+  };
 
   componentDidMount() {
     this.setState({ data: dummyData });
   }
 
   render() {
-    return (
-      <div className="app">
-        <header className="searchBar">
-          <SearchBar />
-        </header>
+    <div className="app">
+      <header className="searchBar">
+        <SearchBar />
+      </header>
 
-        <div className="postContainer">
-          <PostContainer post={this.state.data} />
-        </div>
+      <div className="postContainer">
+        <PostContainer data={this.state.data} />
       </div>
-    );
+    </div>;
   }
 }
-
 export default App;
