55f882c5e0cf9f8a3167c07dcaf5f3d11523683a
Carlossantana987 <carlossantana987@outlook.com>
2019-06-05 16:18:35 -0400

    app.js components moved to PostsPage component

--- insta-clone/src/App.js
+++ insta-clone/src/App.js
@@ -1,50 +1,10 @@
 import React from "react";
 import "./App.scss";
-import dummyData from "./dummy-data";
-import SearchBar from "./components/SearchBar/SearchBar";
-import PostContainer from "./components/PostContainer/PostContainer";
+import PostsPage from "./components/PostContainer/PostsPage";
 
 class App extends React.Component {
-  state = {
-    data: [],
-    filteredPosts: []
-  };
-
-  changeHandler = e => {
-    this.setState({
-      [e.target.name]: e.target.value
-    });
-  };
-
-  componentDidMount() {
-    this.setState({ data: dummyData });
-  }
-  searchFilter = e => {
-    const filtered = this.state.data.filter(post =>
-      post.username.includes(e.target.value)
-    );
-    this.setState({ filteredPosts: filtered });
-  };
-
   render() {
-    return (
-      <div className="app">
-        <header className="searchBar">
-          <SearchBar
-            newSearch={this.state.search}
-            searchFilter={this.searchFilter}
-          />
-        </header>
-
-        <div className="postContainer">
-          <PostContainer
-            data={this.state.data}
-            filteredPosts={this.state.filteredPosts}
-            searchFilter={this.searchFilter}
-          />
-        </div>
-      </div>
-    );
+    return <PostsPage />;
   }
 }
 export default App;
