3c3aaa8aaf684bd8d21ce4756e6a3e60d373a031
Carlossantana987 <carlossantana987@outlook.com>
2019-06-06 16:01:44 -0400

    search bar re-fractored to use styled-components

--- insta-clone/src/components/SearchBar/SearchBar.js
+++ insta-clone/src/components/SearchBar/SearchBar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./SearchBar.scss";
+import styled from "styled-components";
 import instagramLogo from "../Login/instagramLogo.png";
 import { faInstagram } from "@fortawesome/free-brands-svg-icons";
 import { faCompass } from "@fortawesome/free-regular-svg-icons";
@@ -7,16 +8,46 @@ import { faHeart } from "@fortawesome/free-regular-svg-icons";
 import { faUser } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+const TopBar = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  margin: 10px 0px;
+`;
+
+const NavLeft = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const InstaLogo = styled.div`
+  padding: 0px 10px 0px 0px;
+  border-right: 1px solid black;
+`;
+
+const InstagramTitle = styled.img`
+  margin: 5px 0px 0px 10px;
+  padding: 0;
+  height: 60px;
+`;
+
+const NavRight = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 15%;
+`;
+
 class SearchBar extends React.Component {
   render() {
     return (
-      <div className="topBar">
-        <div className="navLeft">
-          <div className="instaLogo">
+      <TopBar>
+        <NavLeft>
+          <InstaLogo>
             <FontAwesomeIcon icon={faInstagram} size="3x" />
-          </div>
-          <img src={instagramLogo} alt="instagram" />
-        </div>
+          </InstaLogo>
+          <InstagramTitle src={instagramLogo} alt="instagram" />
+        </NavLeft>
 
         <div className="navCener">
           <input
@@ -27,18 +58,14 @@ class SearchBar extends React.Component {
           />
         </div>
 
-        <div className="navRight">
-          <div className="icon">
-            <FontAwesomeIcon icon={faCompass} size="3x" />
-          </div>
-          <div className="icon">
-            <FontAwesomeIcon icon={faHeart} size="3x" />
-          </div>
-          <div className="icon">
-            <FontAwesomeIcon icon={faUser} size="3x" />
-          </div>
-        </div>
-      </div>
+        <NavRight>
+          <FontAwesomeIcon icon={faCompass} size="3x" />
+
+          <FontAwesomeIcon icon={faHeart} size="3x" />
+
+          <FontAwesomeIcon icon={faUser} size="3x" />
+        </NavRight>
+      </TopBar>
     );
   }
 }
