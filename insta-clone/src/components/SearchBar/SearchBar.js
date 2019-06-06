af3daaa2ebc6c39bec3cbe3086069c759cecbca5
Carlossantana987 <carlossantana987@outlook.com>
2019-06-06 02:14:20 -0400

    styled searchBar

--- insta-clone/src/components/SearchBar/SearchBar.js
+++ insta-clone/src/components/SearchBar/SearchBar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./SearchBar.scss";
+import instagramLogo from "../Login/instagramLogo.png";
 import { faInstagram } from "@fortawesome/free-brands-svg-icons";
 import { faCompass } from "@fortawesome/free-regular-svg-icons";
 import { faHeart } from "@fortawesome/free-regular-svg-icons";
@@ -10,19 +11,32 @@ class SearchBar extends React.Component {
   render() {
     return (
       <div className="topBar">
-        <div className="logo">
-          <FontAwesomeIcon icon={faInstagram} /> | Instagram
+        <div className="navLeft">
+          <div className="instaLogo">
+            <FontAwesomeIcon icon={faInstagram} size="3x" />
+          </div>
+          <img src={instagramLogo} alt="instagram" />
         </div>
-        <input
-          type="text"
-          onChange={this.props.searchFilter}
-          name="search"
-          placeholder="search"
-        />
-        <div className="navIcon">
-          <FontAwesomeIcon icon={faCompass} />
-          <FontAwesomeIcon icon={faHeart} />
-          <FontAwesomeIcon icon={faUser} />
+
+        <div className="navCener">
+          <input
+            type="text"
+            onChange={this.props.searchFilter}
+            name="search"
+            placeholder="search"
+          />
+        </div>
+
+        <div className="navRight">
+          <div className="icon">
+            <FontAwesomeIcon icon={faCompass} size="3x" />
+          </div>
+          <div className="icon">
+            <FontAwesomeIcon icon={faHeart} size="3x" />
+          </div>
+          <div className="icon">
+            <FontAwesomeIcon icon={faUser} size="3x" />
+          </div>
         </div>
       </div>
     );
